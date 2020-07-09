b006c4ae8c141179d3b015540561d59b67c368c3
Max Mitrani <mjmitrani168@gmail.com>
2020-07-09 07:43:07 -0600

    small rspec refactor

--- spec/features/admin/admin_can_create_tutorials_spec.rb
+++ spec/features/admin/admin_can_create_tutorials_spec.rb
@@ -12,6 +12,6 @@ describe 'An admin user can add tags to tutorials' do
     fill_in'tutorial[thumbnail]', with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
     click_on "Save"
     visit admin_dashboard_path
-  
+    expect(page).to have_content("Eating Food")
   end
 end
